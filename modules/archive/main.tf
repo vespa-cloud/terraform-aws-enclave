9b120a735c88b34577979ef10051cc9cba8bc354
Valerij Fredriksen <valerijf@yahooinc.com>
2023-02-15 11:17:05 +0100

    Set required principal

--- modules/archive/main.tf
+++ modules/archive/main.tf
@@ -105,6 +105,11 @@ data "aws_iam_policy_document" "archive" {
 
     effect = "Allow"
 
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
     actions = [
       "s3:PutObject",
       "s3:PutObjectTagging"
