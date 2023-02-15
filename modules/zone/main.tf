5310f1e4f4602884a7cb6984a77dac9beecb159c
Øyvind Grønnesby <oyving@yahooinc.com>
2023-02-15 11:42:38 +0100

    Allow root to update key policy

--- modules/zone/main.tf
+++ modules/zone/main.tf
@@ -357,18 +357,7 @@ data "aws_iam_policy_document" "ebs_key" {
     effect = "Allow"
 
     actions = [
-      "kms:Create*",
-      "kms:Describe*",
-      "kms:Enable*",
-      "kms:List*",
-      "kms:Put*",
-      "kms:Update*",
-      "kms:Revoke*",
-      "kms:Disable*",
-      "kms:Get*",
-      "kms:Delete*",
-      "kms:ScheduleKeyDeletion",
-      "kms:CancelKeyDeletion"
+      "kms:PutKeyPolicy"
     ]
 
     principals {
