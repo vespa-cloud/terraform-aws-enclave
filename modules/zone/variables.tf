98e4bc18f398f7ed1fb84cfeaeef627900ae2865
Øyvind Grønnesby <oyving@yahooinc.com>
2023-08-16 09:23:38 +0200

    Enforce that zones must have IP ranges within 10.0.0.0/8

--- modules/zone/variables.tf
+++ modules/zone/variables.tf
@@ -11,12 +11,16 @@ variable "zone" {
   })
 }
 
+locals {
+  valid_ipv4_subnets = tolist([for x in range(0, 256) : cidrsubnet("10.0.0.8/8", 8, x)])
+}
+
 variable "zone_ipv4_cidr" {
   description = "CIDR for zone network"
   type        = string
   default     = "10.128.0.0/16"
   validation {
-    condition = try(cidrnetmask(var.zone_ipv4_cidr), null) == "255.255.0.0" && !contains(cidrsubnets("172.16.0.0/15", 1, 1), var.zone_ipv4_cidr)
-    error_message = "CIDR for the zone network must be /16 and cannot overlap with 172.16.0.0/15"
+    condition = try(cidrnetmask(var.zone_ipv4_cidr), null) == "255.255.0.0" && contains(locals.valid_ipv4_subnets, var.zone_ipv4_cidr)
+    error_message = "CIDR for the zone network must be /16 and must be within 10.0.0.0/8"
   }
 }
