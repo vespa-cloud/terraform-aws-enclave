4291e49d19e2db6415d7b4dc9f645a8a441af071
Eirik Nygaard <eirik.nygaard@yahooinc.com>
2024-06-18 13:14:02 +0200

    Expose azid mapping

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,12 @@
 
 locals {
   template_version = "1.0.6"
-  az_by_region = {
-    aws-us-east-1c      = "use1-az6",
-    aws-us-west-2a      = "usw2-az1",
-    aws-eu-west-1a      = "euw1-az2",
-    aws-ap-northeast-1a = "apne1-az4",
-  }
   zones_by_env = {
     for zone in var.all_zones :
     zone.environment => merge(
       {
         name             = "${zone.environment}.${zone.region}",
-        az               = local.az_by_region[zone.region],
+        az               = var.az_by_region[zone.region],
         template_version = local.template_version,
       },
       zone
