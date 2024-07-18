14bb3f493362a742985f2cf0b81818ee9c836609
Valerij Fredriksen <valerijf@vespa.ai>
2024-07-18 11:37:38 +0200

    Use SSM document in system account

--- modules/ssh/main.tf
+++ modules/ssh/main.tf
@@ -45,8 +45,8 @@ resource "aws_iam_policy" "vespa_ssh_login_policy" {
         ]
         Resource = [
           "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:instance/*",
+          "arn:aws:ssm:*:${var.vespa_cloud_account}:document/Vespa-UpdatePublicSshKey",
           "arn:aws:ssm:*::document/AWS-StartSSHSession",
-          aws_ssm_document.update_public_ssh_key.arn,
         ]
       },
       {
@@ -56,61 +56,6 @@ resource "aws_iam_policy" "vespa_ssh_login_policy" {
       }
     ]
   })
-  tags = {
-    managedby = "vespa-cloud"
-  }
-}
-
-resource "aws_ssm_document" "update_public_ssh_key" {
-  name          = "Vespa-UpdatePublicSshKey"
-  document_type = "Command"
-
-  content = jsonencode({
-    schemaVersion = "2.2",
-    description   = "Temporarily adds public SSH key to the instance",
-    parameters = {
-      lifetime = {
-        type           = "String"
-        description    = "Duration, in seconds, they public key stays on the instance before it is deleted"
-        default        = "10"
-        allowedPattern = "^[0-9]+$"
-      }
-      publicKey = {
-        type           = "String"
-        description    = "SSH public key"
-        allowedPattern = "^[a-zA-Z0-9+/ @-]+$"
-      }
-      user = {
-        type           = "String"
-        description    = "SSH username"
-        default        = "ec2-user"
-        allowedPattern = "^[a-z0-9-]+$"
-      }
-      operator = {
-        type           = "String"
-        description    = "Name of the operator"
-        allowedPattern = "^[a-z0-9-]+$"
-      }
-    }
-
-    mainSteps = [
-      {
-        name   = "updatePublicSshKey"
-        action = "aws:runShellScript"
-        inputs = {
-          runCommand = [
-            "#!/bin/bash",
-            "keysdir=/etc/ssh/authorized_keys.d/{{user}}",
-            "keyfile=$keysdir/{{operator}}.$RANDOM.$$",
-            "test -d $keysdir || install -d -o root -g root -m 755 $keysdir",
-            "install -o root -g root -m 644 <(printf \"%s\\n\" \"{{publicKey}}\") $keyfile",
-            "nohup bash -c \"sleep {{lifetime}}; rm -f $keyfile\" &> /dev/null &"
-          ]
-        }
-      }
-    ]
-  })
-
   tags = {
     managedby = "vespa-cloud"
   }
