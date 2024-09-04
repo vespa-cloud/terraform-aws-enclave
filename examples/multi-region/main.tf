074ce2a59cfd4b80e6baee285d0bc79fc43911ee
Kristian Aune <kkraune@users.noreply.github.com>
2024-09-04 13:44:29 +0200

    add perf zone

--- examples/multi-region/main.tf
+++ examples/multi-region/main.tf
@@ -31,12 +31,21 @@ module "enclave" {
 #
 
 #
-# Set up the VPC that contains the Enclaved Vespa appplication's dev zone.
+# Set up the VPC that contains the Enclaved Vespa appplication's dev and perf zones.
 #
 module "zone_dev_us_east_1c" {
   source  = "vespa-cloud/enclave/aws//modules/zone"
   version = ">= 1.0.0, < 2.0.0"
   zone    = module.enclave.zones.dev.aws_us_east_1c
+  providers = {
+    aws = aws.us_east_1
+  }
+}
+
+module "zone_perf_us_east_1c" {
+  source  = "vespa-cloud/enclave/aws//modules/zone"
+  version = ">= 1.0.0, < 2.0.0"
+  zone    = module.enclave.zones.perf.aws_us_east_1c
   providers = {
     aws = aws.us_east_1
   }
