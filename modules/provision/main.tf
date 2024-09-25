b3a8df197952910f187ba25a3215a97d4d91fbd4
Morten Tokle <mortent@vespa.ai>
2024-09-25 08:59:40 +0200

    Allow ec2:DescribeInstanceTypeOfferings

--- modules/provision/main.tf
+++ modules/provision/main.tf
@@ -93,6 +93,7 @@ data "aws_iam_policy_document" "provision_policy" {
       "ec2:DescribeImages",
       "ec2:DescribeInstanceStatus",
       "ec2:DescribeInstances",
+      "ec2:DescribeInstanceTypeOfferings",
       "ec2:DescribeInternetGateways",
       "ec2:DescribeNetworkInterfaces",
       "ec2:DescribeRouteTables",
