901bbf1493d47a9e2aed673ae76c7cc9e0318f2c
Eirik Nygaard <eirik.nygaard@yahooinc.com>
2025-03-06 20:59:07 +0100

    Identifiers must be a list

--- modules/archive/main.tf
+++ modules/archive/main.tf
@@ -161,7 +161,7 @@ data "aws_iam_policy_document" "kms_archive" {
     effect = "Allow"
     principals {
       type        = "AWS"
-      identifiers = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
     }
     actions = [
       "kms:*"
