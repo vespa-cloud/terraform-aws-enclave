9bab38100e54cbf7cf858001e9b566bf47f75763
evgiz <sigve@vespa.ai>
2025-08-20 09:56:25 +0200

    Fix formatting

--- outputs.tf
+++ outputs.tf
@@ -29,6 +29,5 @@ output "vespa_cloud_account" {
 
 output "vespa_host_role" {
   description = "The AWS role assigned to Vespa Cloud hosts"
-  value = "vespa.tenant.${var.tenant_name}.aws-${data.aws_caller_identity.current.account_id
-  }.tenant-host-service"
+  value       = "vespa.tenant.${var.tenant_name}.aws-${data.aws_caller_identity.current.account_id}.tenant-host-service"
 }
