b6e344cb5dbe359cf4a5bd5761de2f058086b2b0
Ola Aunrønning <olaa@vespa.ai>
2026-06-30 13:01:58 +0200

    Add prod.aws-eu-west-1

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
   # NOTE: Do not rename or move this variable!
   # Used by github actions to tag releases. Bump for non-trivial changes.
-  template_version = "1.8.4"
+  template_version = "1.8.5"
 }
 
 terraform {
