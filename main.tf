9bf4bc825cc290de54236d42a0fec1f23906118b
Ola Aunrønning <olaa@vespa.ai>
2026-07-09 10:40:05 +0200

    Add prod.aws-use2-az2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
   # NOTE: Do not rename or move this variable!
   # Used by github actions to tag releases. Bump for non-trivial changes.
-  template_version = "1.8.6"
+  template_version = "1.8.7"
 }
 
 terraform {
